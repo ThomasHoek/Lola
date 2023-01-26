9578f34fa8b28dc57954d5d95e39c870c3c5af2c
Thomas Hoek <hoek.thomas@gmail.com>
2023-01-26 12:21:07 +0100

    comments

--- models/ccg_classes.py
+++ models/ccg_classes.py
@@ -1,44 +1,88 @@
+from __future__ import annotations
+
+
 class tree_info():
     """
     tree class to parse the NLTK format into something usable
     """
 
-    def __init__(self, left, right, label):
-        self.left = left
-        self.right = right
-        self.label = label
+    def __init__(self,
+                 left: tree_info | leaf_info,
+                 right: tree_info | leaf_info,
+                 label: str) -> None:
+        self.left: tree_info | leaf_info = left
+        self.right: tree_info | leaf_info = right
+        self.label: str = label
 
-    def print_lambda(self):
-        left = self.left.print_lambda()
-        right = self.right.print_lambda()
+    def make_lambda(self, lambda_formule: bool = True) -> str:
+        """
+        make_lambda transforms the tree structure into a lambda string
+
+        Recursively builds down the tree into the leaf structures finding the lambda/word.
+        Then builds back the lambda expression making use of the label / ccg information.
+        Returns all the info back into a string of the tree and children properly parsed.
+
+        Args:
+            lambda_formule (bool, optional): if false prints the word itself
+                                             if true prints the lambda formule.
+                                             Defaults to True.
+
+        Returns:
+            str: output string
+        """
+        left: str = self.left.make_lambda(lambda_formule=lambda_formule)
+        right: str = self.right.make_lambda(lambda_formule=lambda_formule)
 
         if self.label == ">":
-            output_str = rf"({left}@{right})"
+            return rf"({left}@{right})"
 
         elif self.label == "<":
-            output_str = rf"({right}@{left})"
+            return rf"({right}@{left})"
 
         elif self.label == ">B":
-            output_str = rf"(\x.{left}@({right}@x))"
+            return rf"(\x.{left}@({right}@x))"
 
         elif self.label == "<B":
-            output_str = rf"(\x.{right}@({left}@x))"
+            return rf"(\x.{right}@({left}@x))"
 
         elif self.label == "<Bx":
-            output_str = rf"(\x.{right}@({left}@x))"
+            return rf"(\x.{right}@({left}@x))"
 
         elif self.label == ">Bx":
-            output_str = rf"(\x.{right}@({left}@x))"
+            return rf"(\x.{right}@({left}@x))"
+        else:
+            raise NotImplementedError(self.label, " not found")
 
-        return output_str
+    def print_lambda(self) -> str:
+        """
+        print_lambda Function to print the lambda rule
 
-    def print(self, depth=0):
-        tab_str = '\t' * depth
-        return f"""label: {self.label}
-    {tab_str}left: {self.left.print(depth + 1)}
-    {tab_str}right: {self.right.print(depth + 1)}"""
+        Returns:
+            str: output string
+        """
+        return self.make_lambda(lambda_formule=False)
 
-    def __repr__(self):
+    def print(self, depth: int = 0) -> str:
+        """
+        print Prints the tree's down in the linux LS format
+
+        prints the trees down using depth in tabs where trees give info about:
+            label, left tree and right tree
+        and nodes give info about:
+            word and semantics
+
+        Args:
+            depth (int, optional): Used to print the depth of the lower trees/nodes. Defaults to 0.
+
+        Returns:
+            str: returns a string of all the information
+        """
+        tab_str: str = '\t' * depth
+        return f"label: {self.label}\n \
+    {tab_str}left: {self.left.print(depth + 1)}\n \
+    {tab_str}right: {self.right.print(depth + 1)}"
+
+    def __repr__(self) -> str:
         return f"[left: {self.left.print()}, right: {self.right.print()}, label: {self.label}]"
 
 
@@ -47,18 +91,70 @@ class leaf_info():
     leaf class to store info from the  NLTK format
     """
 
-    def __init__(self, tree):
-        self.semantics, _ = tree.label()
+    def __init__(self, tree) -> None:  # type: ignore
+        """
+        __init__ Parses a NLTK tree into leafs
+
+        Args:
+            tree (NLTK.tree.tree.tree): NLTK tree structure after parsing
+
+        Sets:
+            self.semantics (str): Info about semantics
+            self.word (str): info about the word
+        """
+        self.semantics: str
+        self.semantics, _ = tree.label()  # type: ignore
+        child: list[str]
         for child in tree:
             for child2 in child:
-                self.word = child2
+                self.word: str = child2
 
-    def print_lambda(self):
-        return f"[{self.word}]"
+    def set_lambda(self, lambda_formule: str) -> None:
+        """
+        set_lambda Set the lambda string that fits the word and type
+        Args:
+            lambda_formule (str): sets the lambda formula of the leaf class
+        """
+        self.lambda_formule: str = lambda_formule
 
-    def print(self, depth=0):
-        tab_str = '\t' * depth
+    def make_lambda(self, lambda_formule: bool = True) -> str:
+        """
+        make_lambda transforms the tree structure into a lambda string
+
+        Recursively builds down the tree into the leaf structures finding the lambda/word.
+        Then builds back the lambda expression making use of the label / ccg information.
+        Returns all the info back into a string of the tree and children properly parsed.
+
+        Args:
+            lambda_formule (bool, optional): if false prints the word itself
+                                             if true prints the lambda formule.
+                                             Defaults to True.
+
+        Returns:
+            str: output string
+        """
+        if lambda_formule:
+            return f"[{self.lambda_formule}]"
+        else:
+            return f"[{self.word}]"
+
+    def print(self, depth: int = 0) -> str:
+        """
+        print Prints the tree's down in the linux LS format
+
+        prints the trees down using depth in tabs where trees give info about:
+            label, left tree and right tree
+        and nodes give info about:
+            word and semantics
+
+        Args:
+            depth (int, optional): Used to print the depth of the lower trees/nodes. Defaults to 0.
+
+        Returns:
+            str: returns a string of all the information
+        """
+        tab_str: str = '\t' * depth
         return f"{tab_str}Word: {self.word} & sem: {self.semantics}"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.word
--- models/parse_ccg.py
+++ models/parse_ccg.py
@@ -1,3 +1,4 @@
+# type: ignore | pylance
 # import nltk.tree.tree
 # from nltk.ccg.lexicon import Token as token_class_type
 from nltk.tree.tree import Tree as tree_class_type
@@ -6,6 +7,15 @@ from parse_tree import parse_tree
 
 
 def recursive_leaves(self):
+    """
+    recursive_leaves Function to recursively parse the NLTK format
+
+    The normal NLTK functions iterates using a for loop.
+    This function reads the NLTK format and parses it into our classes of a tree and node.
+
+    Returns:
+        tree_info: self made class that stores information in a tree, leaf structure
+    """
     leaf_lst = []
 
     # if another tree
@@ -16,13 +26,16 @@ def recursive_leaves(self):
 
         return tree_info(first, second, label)
 
+    # tree only contains nodes
     else:
-        # tree only contains nodes
+        # TODO: this might break for type-raising
+        # but that has yet to be implemented
         return leaf_info(self)
 
     return leaf_lst
 
 
+# add function to NLTK library
 tree_class_type.recursive_leaves = recursive_leaves
 
 
@@ -47,8 +60,11 @@ ccg(84,
    t(n, 'mensen', [lemma:'mensen'])),
   t(s:dcl\np, 'lopen', [lemma:'lopen']))).
 '''
-
+    # get data from laura
     parse = parse_tree("Een man zit in een veld", data)
-    test_var = parse.recursive_leaves()
-    print(test_var.print())
-    print(test_var.print_lambda())
+
+    # parse data into tree/leaf format
+    tree_parse = parse.recursive_leaves()
+
+    print(tree_parse.print())
+    print(tree_parse.print_lambda())
